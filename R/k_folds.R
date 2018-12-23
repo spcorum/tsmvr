f602b7cb4b0ab53a2d56bd43a324f002c33aebff
spcorum <spcorum@gmail.com>
2018-12-22 20:01:43 -0600

    Added testing for k_folds.R

--- R/k_folds.R
+++ R/k_folds.R
@@ -3,9 +3,9 @@
 #' Produces k-fold partitions of training and validation indices for
 #' a dataset.
 #'
-#' @param n number of data points (positive integer)
-#' @param k number of k-folds (integer > 1)
-#' @param seed sets random seed for reproducibility (positive integer)
+#' @param n number of data points (integer valued numeric > 1)
+#' @param k number of k-folds (integer valued numeric > 1)
+#' @param seed sets random seed for reproducibility (numeric)
 #' @return A list of length two, each entry itself being a list of
 #' length \code{K} containing the training (validation) indices.
 #'
@@ -14,11 +14,15 @@
 #' @export
 k_folds <- function(n, k, seed = NULL) {
 
+  stopifnot(
+    n %% 1 == 0, n > 1, k %% 1 == 0, k > 1
+  )
+
   # Create random permutation of the data indices.
   set.seed(seed)
   full_list <- sample.int(n)
 
-  # Calculate the cv-fold indeces by making K evenly sized splits of
+  # Calculate the cv-fold indices by making K evenly sized splits of
   # the permuted indices.
   cv_list <- split(full_list, ceiling(seq_along(full_list) / (n / k)))
   names(cv_list) <- NULL
